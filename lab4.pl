bec1edddc34d3f54b787ce6c919e3d69c304616a
mmaz <marta@mazurkiewi.cz>
2021-11-05 10:04:39 +0100

    Added len1 predicate

--- lab4.pl
+++ lab4.pl
@@ -4,3 +4,6 @@ concat1([],L2,L2).
 concat1([Head|Tail],X,[Head|Y]) :- concat1(Tail,X,Y). 
 delete1(Head,[Head|Tail],Tail).
 delete1(X,[Y|Tail],[Y|L1]):-delete1(X,Tail,L1).
+
+len1([],zero).
+len1([_|Tail],s(X)):-len1(Tail,X).
